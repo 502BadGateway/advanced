399c63650886e0bef5910d3b76cf4db672b97f24
Sam Barratt <milktheelephant@gmail.com>
2014-11-10 23:52:37 +0000

    Did stuff ready to finish robot. Not working yet, done for tonight.

--- object_behaviours.py
+++ object_behaviours.py
@@ -8,3 +8,26 @@ import random
 def invalid(robot, x,y):        #If the obstacle referance was invalid then bail out, because something went wrong in the generation.
     print "Invalid obstacle referance. Something went wrong, bailing out..."
     quit()
+
+def regular (robot, x,y):       #If there is a regular object the turn to a random direction 
+    random_direction()
+    return
+
+def large (robot, x,y):         #Pretty much the same as the regular object. we dont need to behave any differently really.
+    random_direction()
+    return
+
+def light (robot, x,y):
+    #TODO Can't do this yet, waiting on Phil to impliment traffic light things.
+    return
+
+
+def mud (robot, x, y):
+    decrease_speed()
+    return
+
+def boost(robot, x,y):
+    increase_speed()
+    return
+
+
--- robot.py
+++ robot.py
@@ -1,6 +1,22 @@
+#Sam B Wrote this :D
+
+#Robot directions:
+#           0
+#           ^
+#           |
+#       3 <- -> 1
+#           |
+#           V
+#           2
+#
+
+
+
+
 from Tkinter import *
 import time
 import random
+import object_behaviours
 
 
 
@@ -13,7 +29,7 @@ def decrease_speed():
     return speed
 
 def random_direction(direction):        #Chooses a random direction for the robot. Considers a given direction if given.
-    new_direction = random.randint(0,4)
+    new_direction = random.randint(0,3)
 
     if new_direction == direction:     #If we managed to choose the direction it said not too, choose another.
         random_direction(direction)
@@ -21,18 +37,63 @@ def random_direction(direction):        #Chooses a random direction for the robo
         return new_direction
 
 def spawn_robot(arena):                  #Chooses a random place to spawn the robot.
-   x_coord0 = random.randint(0,24)
-   y_coord0 = random.randint(0,24)
-   x_coord1 = x_coord0 + 20
-   y_coord1 = y_coord0 + 20
-   if arena_list[x_coord0 * 20][y_coord0*20] != 0:
-       spawn_robot()
-   else:
-	robot = arena.create_oval(x_coord0, y_coord0, x_coord1, y_coord1, outline="green", fill="green", width=1)
+    x_coord0 = random.randint(0,24)
+    y_coord0 = random.randint(0,24)
+    x_coord1 = x_coord0 + 20
+    y_coord1 = y_coord0 + 20
+    if arena_list[x_coord0 * 20][y_coord0*20] != 0:      #If there is something in the element. Dont spawn there.
+        spawn_robot()
+    else:
+        robot = arena.create_oval(x_coord0, y_coord0, x_coord1, y_coord1, outline="green", fill="green", width=1)
+return robot                            #Returns Robot ID for moving it.
 
 
-
-
+options {                   #Dictionary containing the object numbers and the functions assotiated with them.
+        0:invalid,          #All these functions are defined in object_behaviours.py
+        1:regular,
+        2:large,
+        3:light,
+        4:mud,
+        5:boost
+        }
 
 def check(direction, x, y):
+    if direction == 0:    # If the direction is upward...  
+        check_y = (y /20) - 1
+        check_x = (x /20) #Then check upward!
+        if arena_list[check_x][check_y] != 0:
+            options[arena_list[check_x][check_y] ()
+        else:
+        return
+    if direction == 1:    # If the direction is right...  
+        check_y = (y /20) 
+        check_x = (x /20) + 1 #Then check right!
+        if arena_list[check_x][check_y] != 0:
+            options[arena_list[check_x][check_y] ()
+        else:
+        return
+    if direction == 2:    # If the direction is right...  
+        check_y = (y /20) + 1 
+        check_x = (x /20) #Then check right!
+        if arena_list[check_x][check_y] != 0:
+            options[arena_list[check_x][check_y] ()
+        else:
+        return
+    if direction == 3:    # If the direction is right...  
+        check_y = (y /20)  
+        check_x = (x /20) - 1 #Then check right!
+        if arena_list[check_x][check_y] != 0:
+            options[arena_list[check_x][check_y] ()
+        else:
+        return
+        
 
+def move(robot,direction,  arena):   #Moves the robot
+    if direction == 0:
+        arena.move(robot, 0, -20)
+    elif direction == 1:
+        arena.move(robot, 20, 0)
+   elif direction == 2:
+        arena.move(robot, 0, 20)
+   elif direction == 3:
+        arena.move(robot, -20, 0)
