b9c979e704e243fe051b552c7b8098320af6bb79
Sam Barratt <milktheelephant@gmail.com>
2014-11-11 22:42:23 +0000

    Forgot to add in last

--- object_behaviours.py
+++ object_behaviours.py
@@ -3,31 +3,32 @@
 from Tkinter import *
 import time
 import random
+import robot
 
 
-def invalid(robot, x,y):        #If the obstacle referance was invalid then bail out, because something went wrong in the generation.
+def invalid(bot, direction,  x,y):        #If the obstacle referance was invalid then bail out, because something went wrong in the generation.
     print "Invalid obstacle referance. Something went wrong, bailing out..."
     quit()
 
-def regular (robot, x,y):       #If there is a regular object the turn to a random direction 
-    random_direction()
+def regular (bot, direction, x,y):       #If there is a regular object the turn to a random direction 
+    robot.random_direction(direction)
     return
 
-def large (robot, x,y):         #Pretty much the same as the regular object. we dont need to behave any differently really.
-    random_direction()
+def large (bot, direction,  x,y):         #Pretty much the same as the regular object. we dont need to behave any differently really.
+    robot.random_direction(direction)
     return
 
-def light (robot, x,y):
+def light (bot, direction,  x,y):
     #TODO Can't do this yet, waiting on Phil to impliment traffic light things.
     return
 
 
-def mud (robot, x, y):
-    decrease_speed()
+def mud (bot, direction,  x, y):
+    robot.decrease_speed()
     return
 
-def boost(robot, x,y):
-    increase_speed()
+def boost(bot, direction,  x,y):
+    robot.increase_speed()
     return
 
 
