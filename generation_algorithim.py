81289106b2c566e0cfb17d6e39f1f9cb172e7883
philhabell <philhabell@gmail.com>
2014-11-11 20:26:50 +0000

    remove random lines that git made

--- generation_algorithim.py
+++ generation_algorithim.py
@@ -11,11 +11,10 @@ if platform_name == "Darwin":
 else:
 	speed=0.01
 print speed
-<<<<<<< HEAD
 traffic_list=[]
 global 	call_num
 call_num=1
-=======
+
 
 """
 Below is a multi-dimensional list of lists. It is essentially 25 lists constaining 25 elements. 
@@ -23,7 +22,6 @@ This gives us a direct reference to a grid of 25x25.
 Due to the fact our arena is 500px by 500px it means each element represents a 20px x 20px area.
 We can reference any area using the notation "arena_list[x][y] and access the stuff inside.
 """
->>>>>>> FETCH_HEAD
 arena_list = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
 							[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
 							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
