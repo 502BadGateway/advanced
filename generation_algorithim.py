01b264da85f7df22b44fbd6fdb948d97e802e0de
philhabell <philhabell@gmail.com>
2014-11-12 11:56:44 +0000

    added speed 0 to traffic light

--- generation_algorithim.py
+++ generation_algorithim.py
@@ -171,6 +171,8 @@ def traffic_light_change(traffic_x_coord0,traffic_y_coord0):
 	traffic_y_coord1 = traffic_y_coord0 + 1
 
 	obstacle_red_traffic = arena.create_rectangle(traffic_x_coord0, traffic_y_coord0, traffic_x_coord1, traffic_y_coord1, fill = "red", outline = "black")
+	speed1=0
+	
 	# we could either do a pause or change robot speed1 to 0 here 
 	arena.delete(obstacle_red_traffic)
 	arena.update()
