5188f9dee544e007a3e3ef5c9c1a08841cc76d8c
samathy <milktheelephant@gmail.com>
2014-11-12 18:24:54 +0000

    Got some stuff working

--- generation_algorithim.py
+++ generation_algorithim.py
@@ -1,23 +1,22 @@
 from Tkinter import *
 import time
 import random
-import robot
 
-#block bellow checks for what OS the user is using and if it is Darwin (OSX) then it increases the speed1, this is to fix the issue of the robot being very slow on OSX
+#block bellow checks for what OS the user is using and if it is Darwin (OSX) then it increases the speed, this is to fix the issue of the robot being very slow on OSX
 import platform
 platform_name=platform.system()
 print platform_name
 if platform_name == "Darwin":
-	speed=0.001
-	robot.speed1=speed
-	robot.speed2=speed
+    speed1=0.001
+    speed2=0.001
 else:
-	speed=0.01
-	robot.speed1=speed
-	robot.speed2=speed
-print speed1
+    speed2=0.01
+    speed2=0.01
+
+print speed2
+
 traffic_list=[]
-global 	call_num
+global     call_num
 call_num=1
 
 
@@ -28,30 +27,30 @@ Due to the fact our arena is 500px by 500px it means each element represents a 2
 We can reference any area using the notation "arena_list[x][y] and access the stuff inside.
 """
 arena_list = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-    						 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+        					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
 """
 Below is the function to randomly generate a object. 
 The majority of obstacles are generated the same way, just with different conditions.
@@ -67,180 +66,171 @@ Using this top corner, we generate the rest of the coords, by adding 20.
 Finally we generate an object using the coords we created using the x_list and y_list numbers.
 """
 def generate_obstacle(arena_list, arena):
-		x_list = random.randint(0,24)           #
-		y_list = random.randint(0,24)
-		if(arena_list[x_list][y_list] == 0):
-				arena_list[x_list][y_list] = 1
-				x_coord0 = x_list * 20
-				y_coord0 = y_list * 20
-				x_coord1 = x_coord0 + 20
-				y_coord1 = y_coord0 + 20
-				obstacle = arena.create_rectangle(x_coord0, y_coord0, x_coord1, y_coord1, fill = "blue", outline = "black")
-				
-		else:
-				generate_obstacle(arena_list, arena)
-		return arena_list
+    	x_list = random.randint(0,24)           #
+    	y_list = random.randint(0,24)
+    	if(arena_list[x_list][y_list] == 0):
+    			arena_list[x_list][y_list] = 1
+    			x_coord0 = x_list * 20
+    			y_coord0 = y_list * 20
+    			x_coord1 = x_coord0 + 20
+    			y_coord1 = y_coord0 + 20
+    			obstacle = arena.create_rectangle(x_coord0, y_coord0, x_coord1, y_coord1, fill = "blue", outline = "black")
+    			
+    	else:
+    			generate_obstacle(arena_list, arena)
+    	return arena_list
 
 def generate_big_obstacle(arena_list, arena):
-		x_list = random.randint(0,24)
-		y_list = random.randint(0,24)
-		if(x_list < 23 and y_list < 23 and arena_list[x_list][y_list] == 0 and arena_list[x_list + 1][y_list] == 0 and arena_list[x_list][y_list + 1] == 0 and arena_list[x_list + 1][y_list + 1] == 0):
-				arena_list[x_list][y_list] = 2
-				arena_list[x_list + 1][y_list] = 2
-				arena_list[x_list][y_list + 1] = 2
-				arena_list[x_list + 1][y_list + 1] = 2
-				x_coord0 = x_list * 20
-				y_coord0 = y_list * 20
-				x_coord1 = x_coord0 + 40
-				y_coord1 = y_coord0 + 40
-				obstacle = arena.create_rectangle(x_coord0, y_coord0, x_coord1, y_coord1, fill = "blue", outline = "black")
-				arena.update()
-				time.sleep(0.1)
-		else:
-				generate_big_obstacle(arena_list, arena)
-		return arena_list
+    	x_list = random.randint(0,24)
+    	y_list = random.randint(0,24)
+    	if(x_list < 23 and y_list < 23 and arena_list[x_list][y_list] == 0 and arena_list[x_list + 1][y_list] == 0 and arena_list[x_list][y_list + 1] == 0 and arena_list[x_list + 1][y_list + 1] == 0):
+    			arena_list[x_list][y_list] = 2
+    			arena_list[x_list + 1][y_list] = 2
+    			arena_list[x_list][y_list + 1] = 2
+    			arena_list[x_list + 1][y_list + 1] = 2
+    			x_coord0 = x_list * 20
+    			y_coord0 = y_list * 20
+    			x_coord1 = x_coord0 + 40
+    			y_coord1 = y_coord0 + 40
+    			obstacle = arena.create_rectangle(x_coord0, y_coord0, x_coord1, y_coord1, fill = "blue", outline = "black")
+    			arena.update()
+    			time.sleep(0.1)
+    	else:
+    			generate_big_obstacle(arena_list, arena)
+    	return arena_list
 def generate_traffic_light_object(arena_list,arena,traffic_list):
-	print "traffics light"
-	x_list = random.randint(0,24)
-	y_list = random.randint(0,24)
-	traffic_list.append(x_list)
-	traffic_list.append(y_list)
-	if (x_list< 22 and arena_list[x_list][y_list] == 0):
-		arena_list[x_list][y_list] = 3
-		arena_list[x_list + 1][y_list] = 3
-		arena_list[x_list + 2][y_list] = 3
-		x_coord0 = x_list * 20
-		y_coord0 = y_list * 20
-		x_coord1 = x_coord0 + 20
-		y_coord1 = y_coord0 + 20
-		obstacle = arena.create_rectangle(x_coord0, y_coord0, x_coord1, y_coord1, fill = "green")
-		arena.update()
-	else:
-		generate_traffic_light_object(arena_list,arena,traffic_list)
-	
+    print "traffics light"
+    x_list = random.randint(0,24)
+    y_list = random.randint(0,24)
+    traffic_list.append(x_list)
+    traffic_list.append(y_list)
+    if (x_list< 22 and arena_list[x_list][y_list] == 0):
+    	arena_list[x_list][y_list] = 3
+    	arena_list[x_list + 1][y_list] = 3
+    	arena_list[x_list + 2][y_list] = 3
+    	x_coord0 = x_list * 20
+    	y_coord0 = y_list * 20
+    	x_coord1 = x_coord0 + 20
+    	y_coord1 = y_coord0 + 20
+    	obstacle = arena.create_rectangle(x_coord0, y_coord0, x_coord1, y_coord1, fill = "green")
+    	arena.update()
+    else:
+    	generate_traffic_light_object(arena_list,arena,traffic_list)
+    
 def generate_mud_object(arena_list,arena):
-	print "mud"
-	x_list = random.randint(0,24)
-	y_list = random.randint(0,24)
-	if(x_list < 23 and y_list < 23 and arena_list[x_list][y_list] == 0 and arena_list[x_list + 1][y_list] == 0 and arena_list[x_list][y_list + 1] == 0 and arena_list[x_list + 1][y_list + 1] == 0):
-			arena_list[x_list][y_list] = 4
-			arena_list[x_list + 1][y_list] = 4
-			arena_list[x_list][y_list + 1] = 4
-			arena_list[x_list + 1][y_list + 1] = 4
-			x_coord0 = x_list * 20
-			y_coord0 = y_list * 20
-			x_coord1 = x_coord0 + 40
-			y_coord1 = y_coord0 + 40
-			obstacle = arena.create_rectangle(x_coord0, y_coord0, x_coord1, y_coord1, fill = "brown", outline = "brown")
-			arena.update()
-			time.sleep(0.1)
-	else:
-		generate_mud_object(arena_list,arena)
-def generate_speed1_boost_object(arena_list,arena):
-	print "speed1 boost"
-	x_list = random.randint(0,24)
-	y_list = random.randint(0,24)
-	if (arena_list[x_list][y_list] == 0):
-		arena_list[x_list][y_list] = 5
-		x_coord0 = x_list * 20
-		y_coord0 = y_list * 20
-		x_coord1 = x_coord0 + 20
-		y_coord1 = y_coord0 + 20
-		obstacle = arena.create_rectangle(x_coord0, y_coord0, x_coord1, y_coord1, fill = "orange", outline = "green")
+    print "mud"
+    x_list = random.randint(0,24)
+    y_list = random.randint(0,24)
+    if(x_list < 23 and y_list < 23 and arena_list[x_list][y_list] == 0 and arena_list[x_list + 1][y_list] == 0 and arena_list[x_list][y_list + 1] == 0 and arena_list[x_list + 1][y_list + 1] == 0):
+    		arena_list[x_list][y_list] = 4
+    		arena_list[x_list + 1][y_list] = 4
+    		arena_list[x_list][y_list + 1] = 4
+    		arena_list[x_list + 1][y_list + 1] = 4
+    		x_coord0 = x_list * 20
+    		y_coord0 = y_list * 20
+    		x_coord1 = x_coord0 + 40
+    		y_coord1 = y_coord0 + 40
+    		obstacle = arena.create_rectangle(x_coord0, y_coord0, x_coord1, y_coord1, fill = "brown", outline = "brown")
+    		arena.update()
+    		time.sleep(0.1)
+    else:
+    	generate_mud_object(arena_list,arena)
+def generate_speed_boost_object(arena_list,arena):
+    print "speed boost"
+    x_list = random.randint(0,24)
+    y_list = random.randint(0,24)
+    if (arena_list[x_list][y_list] == 0):
+    	arena_list[x_list][y_list] = 5
+    	x_coord0 = x_list * 20
+    	y_coord0 = y_list * 20
+    	x_coord1 = x_coord0 + 20
+    	y_coord1 = y_coord0 + 20
+    	obstacle = arena.create_rectangle(x_coord0, y_coord0, x_coord1, y_coord1, fill = "orange", outline = "green")
 
-		arena.update()
-	else:
-		generate_speed1_boost_object(arena_list,arena)
+    	arena.update()
+    else:
+    	generate_speed_boost_object(arena_list,arena)
 def genertate_red_traffic_light(arena_list,arena): # this function gives the traffic light locations and it turns the traffic light red
-	print "red"
-	if call_num == 1:
-		traffic_x_coord0 = traffic_list[1]
-		traffic_y_coord0 = traffic_list[2]
+    print "red"
+    if call_num == 1:
+    	traffic_x_coord0 = traffic_list[1]
+    	traffic_y_coord0 = traffic_list[2]
 
-		traffic_light_change(traffic_x_coord0,traffic_y_coord0)
-	elif call_num == 2:
-		traffic_x_coord0 = traffic_list[3]
-		traffic_y_coord0 = traffic_list[4]
+    	traffic_light_change(traffic_x_coord0,traffic_y_coord0)
+    elif call_num == 2:
+    	traffic_x_coord0 = traffic_list[3]
+    	traffic_y_coord0 = traffic_list[4]
+    elif call_num == 3:
+    	traffic_x_coord0 = traffic_list[5]
+    	traffic_y_coord0 = traffic_list[6]
+    else:
+    	print "no more in list"
 
-		traffic_light_change(traffic_x_coord0,traffic_y_coord0)
-	elif call_num == 3:
-		traffic_x_coord0 = traffic_list[5]
-		traffic_y_coord0 = traffic_list[6]
-
-		traffic_light_change(traffic_x_coord0,traffic_y_coord0)
-	else:
-		print "no more in list"
-
-	call_num = call_num + 1
+    call_num = call_num + 1
 def traffic_light_change(traffic_x_coord0,traffic_y_coord0):
-	traffic_x_coord1 = traffic_x_coord0 + 1
-	traffic_y_coord1 = traffic_y_coord0 + 1
+    traffic_x_coord1 = traffic_x_coord0 + 1
+    traffic_y_coord1 = traffic_y_coord0 + 1
 
-	obstacle_red_traffic = arena.create_rectangle(traffic_x_coord0, traffic_y_coord0, traffic_x_coord1, traffic_y_coord1, fill = "red", outline = "black")
-	robot.speed1=0
-	time.sleep(2)#this is temporary untill I find out a better way of doing it
-	# we could either do a pause or change robot speed1 to 0 here 
-	arena.delete(obstacle_red_traffic)
-	robot.speed1=speed
-	arena.update()
+    obstacle_red_traffic = arena.create_rectangle(traffic_x_coord0, traffic_y_coord0, traffic_x_coord1, traffic_y_coord1, fill = "red", outline = "black")
+    # we could either do a pause or change robot speed to 0 here 
+    arena.delete(obstacle_red_traffic)
+    arena.update()
 def advanced_init():
-		window2 = Tk()
-		arena = Canvas(window2, width = 500, height = 500, bg = 'white') # generates a canvas of 500px x 500px for the arena
-		for i in range(0,5):
-			generate_big_obstacle(arena_list, arena)
-			arena.update()
-		for i in range(0,3):
-			generate_mud_object(arena_list,arena)
-			arena.update()
-		for i in range(0,3):
-			generate_traffic_light_object(arena_list,arena,traffic_list)
-			arena.update()
-		for i in range(0,3):
-			generate_speed1_boost_object(arena_list,arena)
-			arena.update()
-		for i in range(0,30):
-			generate_obstacle(arena_list, arena)
-			arena.update()
-		print arena_list
-
-        robot1 = robot.spawn_robot(arena)
-
-		arena.update_idletasks()
-		speed1=0.1
-
+    	window2 = Tk()
+    	arena = Canvas(window2, width = 500, height = 500, bg = 'white') # generates a canvas of 500px x 500px for the arena
+    	for i in range(0,5):
+    		generate_big_obstacle(arena_list, arena)
+    		arena.update()
+    	for i in range(0,3):
+    		generate_mud_object(arena_list,arena)
+    		arena.update()
+    	for i in range(0,3):
+    		generate_traffic_light_object(arena_list,arena,traffic_list)
+    		arena.update()
+    	for i in range(0,3):
+    		generate_speed_boost_object(arena_list,arena)
+    		arena.update()
+    	for i in range(0,30):
+    		generate_obstacle(arena_list, arena)
+    		arena.update()
     	print arena_list
+
+        import robot
+
+        robot1 = robot.Robot()
+        robot1.speed = speed2
+        robot1.spawn_robot(arena)
+        # robot1.spawn_robot(robot1,arena)
+        print arena_list
         
+        direction = 0
 
+        arena.pack()
 
-    	arena.pack()
-
-    	arena.update_idletasks()
-    	speed=0.1
-
-
+        arena.update_idletasks()
         go = True
 
         while go == True:
             print go
-            direction = 0
-            x0,y0,x1,y1 = arena.coords(robot1)
-            if x0 >= 480:
+            robot1.direction = 0
+            if robot1.coords[0] >= 480:
                 go = False
-            elif x0 <= 20:
+            elif robot1.coords[0] <= 20:
                 go = False
-            elif y0 >= 480:
+            elif robot1.coords[1] >= 480:
                 go = False
-            elif y0 <= 20:
+            elif robot1.coords[1] <= 20:
                 go =False
-            robot.check(robot1, direction, int(x0),int(y0))
-            robot.move(robot1, direction, arena)
+            robot1.check()
+            robot1.move(arena)
             arena.update()
             time.sleep(0.1)
         print  arena.coords(robot1)
         print go
 
-    	window2.mainloop() # runs everything
+        window2.mainloop() # runs everything
 
 
 
 
+        robot.direction = direction
--- robot.py
+++ robot.py
@@ -16,90 +16,137 @@
 from Tkinter import *
 import time
 import random
-import object_behaviours
 import generation_algorithim
 
 
-def increase_speed1():       #Increases the speed1
-    speed11 = speed11 *2
-    return speed11            #Yes speed1 is a global, but it might be easier in the future if we return something.
-
-def decrease_speed1():
-    speed1 = speed1 /2
-    return speed1
-
-def random_direction(direction):        #Chooses a random direction for the robot. Considers a given direction if given.
-    new_direction = random.randint(0,3)
-
-    if new_direction == direction:     #If we managed to choose the direction it said not too, choose another.
-        random_direction(direction)
-    else:
-        return new_direction
-
-def spawn_robot(arena):                  #Chooses a random place to spawn the robot.
-    x_list = random.randint(0,24)
-    y_list = random.randint(0,24)
-    x_coord0 = x_list * 20
-    y_coord0 = y_list * 20
-    x_coord1 = x_coord0 + 20 
-    y_coord1 = y_coord0 + 20
-    print len(generation_algorithim.arena_list)
-    if generation_algorithim.arena_list[x_list][y_list] != 0:      #If there is something in the element. Dont spawn there.
-        spawn_robot(arena)
-    else:
-        robot = arena.create_oval(x_coord0, y_coord0, x_coord1, y_coord1, outline="green", fill="green", width=1)
-    return robot                            #Returns Robot ID for moving it.
+class Robot:
 
 
-options = {                   #Dictionary containing the object numbers and the functions assotiated with them.
-        0 : object_behaviours.invalid,          #All these functions are defined in object_behaviours.py
-        1 : object_behaviours.regular,
-        2 : object_behaviours.large,
-        3 : object_behaviours.light,
-        4 : object_behaviours.mud,
-        5 : object_behaviours.boost
-        }
+    speed = 0
+    direction = 0
+    coords = []
+    id = 0
 
-def check(robot, direction, x, y):
-    if direction == 0:    # If the direction is upward...  
-        check_y = (y /20) - 1
-        check_x = (x /20) #Then check upward!
-        if generation_algorithim.arena_list[check_x][check_y] != 0:
-            options[generation_algorithim.arena_list[check_x][check_y]](robot, direction,x, y)
+
+    def __init__(self):
+        return
+
+
+
+    def invalid(self):        #If the obstacle referance was invalid then bail out, because something went wrong in the generation.
+        return 
+
+    def regular (self):       #If there is a regular object the turn to a random direction 
+        self.random_direction(selfdirection)
+        return
+    
+    def large (self):         #Pretty much the same as the regular object. we dont need to behave any differently really.
+        self.random_direction(selfdirection)
+        return
+    
+    def light (self):
+        #TODO Can't do this yet, waiting on Phil to impliment traffic light things.
+        return
+    
+    
+    def mud (self):
+        self.decrease_speed(self)
+        return
+    
+    def boost(self):
+        self.increase_speed(self)
+        return
+
+
+    def increase_speed():       #Increases the speed
+        speed = speed*2
+        return speed            #Yes speed is a global, but it might be easier in the future if we return something.
+    
+    def decrease_speed():
+        speed = speed/2
+        return speed
+    
+    def random_direction(direction):        #Chooses a random direction for the robot. Considers a given direction if given.
+        new_direction = random.randint(0,3)
+    
+        if new_direction == direction:     #If we managed to choose the direction it said not too, choose another.
+            random_direction(direction)
         else:
-            return
-    elif direction == 1:    # If the direction is right...  
-        check_y = (y /20) 
-        check_x = (x /20) + 1 #Then check right!
-        if generation_algorithim.arena_list[check_x][check_y] != 0:
-            options[generation_algorithim.arena_list[check_x][check_y]](robot, direction,x, y)
-        else:
-            return
-    elif direction == 2:    # If the direction is right...  
-        check_y = (y /20) + 1 
-        check_x = (x /20) #Then check right!
-        if generation_algorithim.arena_list[check_x][check_y] != 0:
-            options[generation_algorithim.arena_list[check_x][check_y]](robot, direction,x, y)
-        else:
-            return
-    elif direction == 3:    # If the direction is right...  
-        check_y = (y /20)  
-        check_x = (x /20) - 1 #Then check right!
-        if generation_algorithim.arena_list[check_x][check_y] != 0:
-            options[generation_algorithim.arena_list[check_x][check_y]](robot, direction,x, y)
-        else:
-            return
-        
+            return new_direction
+    
+    def spawn_robot(self,arena):                  #Chooses a random place to spawn the robot.
+        x_list = random.randint(0,24)
+        y_list = random.randint(0,24)
+        x_coord0 = x_list * 20
+        y_coord0 = y_list * 20
+        x_coord1 = x_coord0 + 20 
+        y_coord1 = y_coord0 + 20
+        print len(generation_algorithim.arena_list)
+        if generation_algorithim.arena_list[x_list][y_list] != 0:      #If there is something in the element. Dont spawn there.
+           self.spawn_robot(arena)
 
-def move(robot,direction,  arena):   #Moves the robot 20 spaces to where ever the robot is pointing.
-    if direction == 0:
-        arena.move(robot, 0, -20)
-    elif direction == 1:
-        arena.move(robot, 20, 0)
-    elif direction == 2:
-        arena.move(robot, 0, 20)
-    elif direction == 3:
-        arena.move(robot, -20, 0)
+        else:
+            robot = arena.create_oval(x_coord0, y_coord0, x_coord1, y_coord1, outline="green", fill="green", width=1)
+            self.coords = arena.coords(robot)
+            print self.coords
+            self.id = robot
+        return                            #Returns Robot ID for moving it.
+    
+    
+    
+    def check(self):
+        print self.coords
+        if self.direction == 0:    # If the direction is upward...  
+            check_y = (int(self.coords[0] /20)) - 1
+            check_x = (int(self.coords[1] /20)) #Then check upward!
+            if generation_algorithim.arena_list[check_x][check_y] != 0:
+                print "something under!"
+                if generation_algorithim.arena_list[check_x][check_y] == 1:
+                    self.invalid()
+                elif generation_algorithim.arena_list[check_x][check_y] == 2:
+                    self.large()
+                elif generation_algorithim.arena_list[check_x][check_y] == 3:
+                    self.light()
+                elif generation_algorithim.arena_list[check_x][check_y] == 4:
+                    self.mud()
+                elif generation_algorithim.arena_list[check_x][check_y] == 5:
+                    self.boost()
+            else:
+                return
+        elif direction == 1:    # If the direction is right...  
+            check_y = (int(self.coords[0] /20)) 
+            check_x = (int(self.coords[1] /20)) + 1 #Then check right!
+            if generation_algorithim.arena_list[check_x][check_y] != 0:
+                options[generation_algorithim.arena_list[check_x][check_y]]()
+            else:
+                    return
+        elif direction == 2:    # If the direction is right...  
+            check_y = (int(self.coords[0] /20)) + 1 
+            check_x = (int(self.coords[1] /20)) #Then check right!
+            if generation_algorithim.arena_list[check_x][check_y] != 0:
+                options[generation_algorithim.arena_list[check_x][check_y]]()
+            else:
+                return
+        elif direction == 3:    # If the direction is right...  
+            check_y = (int(self.coords[0] /20))  
+            check_x = (int(self.coords[1] /20)) - 1 #Then check right!
+            if generation_algorithim.arena_list[check_x][check_y] != 0:
+                options[generation_algorithim.arena_list[check_x][check_y]](robot, direction,x, y)
+            else:
+                return
+    
+    
+    def move(self,arena):   #Moves the robot 20 spaces to where ever the robot is pointing.
+        if self.direction == 0:
+            arena.move(self.id, 0, -20)
+        elif self.direction == 1:
+            arena.move(self.id, 20, 0)
+        elif self.direction == 2:
+            arena.move(self.id, 0, 20)
+        elif self.direction == 3:
+            arena.move(self.id, -20, 0)
+        self.coords = arena.coords(self.id) 
+        print "moved"
 
-    print "moved"
 
+       
