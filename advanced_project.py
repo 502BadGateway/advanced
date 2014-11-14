0fdfca4e04532a0abc05ceae280000677facfbda
philhabell <philhabell@gmail.com>
2014-11-14 19:11:03 +0000

    changed shapes for images
    still need to work out why only one image spawns

--- advanced_project.py
+++ advanced_project.py
@@ -32,7 +32,7 @@ def basic():
 	obstacle_rectangle4 = arena.create_rectangle(0, 350, 250, 400, fill = "red", outline = "red")
 	obstacle_rectangle5 = arena.create_rectangle(400, 350, 450, 400, fill = "red", outline = "red")
 	obstacle_rectangle6 = arena.create_rectangle(0, 0, 100, 200, fill = "red", outline = "red")
-	start_area = arena.create_rectangle(0, 450, 50, 500, fill = "#82FA02") # uses a Hexidecimal code for light green
+	start_arena = arena.create_rectangle(0, 450, 50, 500, fill = "#82FA02") # uses a Hexidecimal code for light green
 
 	#flag gif
 	gif1 = PhotoImage(file = 'flag.gif')
@@ -88,11 +88,11 @@ def intermdiate():
 
 	obstacle_rectangle1 = arena.create_rectangle(350, 50, 400, 200, fill = "red", outline = "black") # creates a obstacle to avoid
 	obstacle_rectangle2 = arena.create_rectangle(150, 200, 300, 250, fill = "red", outline = "black")
-	obstacle_rectangle3 = arena.create_rectangle(400, 250, 500, 300, fil1l = "red", outline = "black")
+	obstacle_rectangle3 = arena.create_rectangle(400, 250, 500, 300, fill = "red", outline = "black")
 	obstacle_rectangle4 = arena.create_rectangle(0, 350, 250, 400, fill = "red", outline = "black")
 	obstacle_rectangle5 = arena.create_rectangle(400, 350, 450, 400, fill = "red", outline = "black")
 	obstacle_rectangle6 = arena.create_rectangle(0, 0, 100, 200, fill = "red", outline = "black")
-	start_area = arena.create_rectangle(0, 450, 50, 500, fill = "#82FA02") # uses a Hexidecimal code for light green
+	start_arena = arena.create_rectangle(0, 450, 50, 500, fill = "#82FA02") # uses a Hexidecimal code for light green
 
 	arena.pack()
 	speed1=0.01
--- generation_algorithim.py
+++ generation_algorithim.py
@@ -1,23 +1,25 @@
 from Tkinter import *
 import time
 import random
-
+#import gc
+#gc.disable()
 #block bellow checks for what OS the user is using and if it is Darwin (OSX) then it increases the speed, this is to fix the issue of the robot being very slow on OSX
 import platform
 platform_name=platform.system()
 print platform_name
 if platform_name == "Darwin":
-    speed1=0.001
-    speed2=0.001
+	speed1=0.001
+	speed2=0.001
 else:
-    speed2=0.01
-    speed2=0.01
+	speed2=0.01
+	speed2=0.01
 
 print speed2
 
 traffic_list=[]
-global     call_num
+global call_num
 call_num=1
+global obstacle
 
 
 """
@@ -28,30 +30,30 @@ We can reference any area using the notation "arena_list[x][y] and access the st
 
 """
 arena_list = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
-        					 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+							[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],\
+							 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
 """
 Below is the function to randomly generate a object. 
 The majority of obstacles are generated the same way, just with different conditions.
@@ -67,179 +69,206 @@ Using this top corner, we generate the rest of the coords, by adding 20.
 Finally we generate an object using the coords we created using the x_list and y_list numbers.
 """
 def generate_obstacle(arena_list, arena):
-    	x_list = random.randint(0,24)           #
-    	y_list = random.randint(0,24)
-    	if(arena_list[x_list][y_list] == 0):
-    			arena_list[x_list][y_list] = 1
-    			x_coord0 = x_list * 20
-    			y_coord0 = y_list * 20
-    			x_coord1 = x_coord0 + 20
-    			y_coord1 = y_coord0 + 20
-    			obstacle = arena.create_rectangle(x_coord0, y_coord0, x_coord1, y_coord1, fill = "blue", outline = "black")
-    			
-    	else:
-    			generate_obstacle(arena_list, arena)
-    	return arena_list
+		x_list = random.randint(0,24)           
+		y_list = random.randint(0,24)
+		if(arena_list[x_list][y_list] == 0):
+				arena_list[x_list][y_list] = 1
+				x_coord0 = x_list * 20
+				y_coord0 = y_list * 20
+				x_coord1 = x_coord0 + 20
+				y_coord1 = y_coord0 + 20
+				global small_obstacle
+				small_obstacle = PhotoImage(file = 'obstacle.gif')
+				obstacle = arena.create_image(x_coord0, y_coord0, image = small_obstacle, anchor = NW)
+				arena.update()
+		else:
+				generate_obstacle(arena_list, arena)
+		return arena_list
 
 def generate_big_obstacle(arena_list, arena):
-    	x_list = random.randint(0,24)
-    	y_list = random.randint(0,24)
-    	if(x_list < 23 and y_list < 23 and arena_list[x_list][y_list] == 0 and arena_list[x_list + 1][y_list] == 0 and arena_list[x_list][y_list + 1] == 0 and arena_list[x_list + 1][y_list + 1] == 0):
-    			arena_list[x_list][y_list] = 2
-    			arena_list[x_list + 1][y_list] = 2
-    			arena_list[x_list][y_list + 1] = 2
-    			arena_list[x_list + 1][y_list + 1] = 2
-    			x_coord0 = x_list * 20
-    			y_coord0 = y_list * 20
-    			x_coord1 = x_coord0 + 40
-    			y_coord1 = y_coord0 + 40
-    			obstacle = arena.create_rectangle(x_coord0, y_coord0, x_coord1, y_coord1, fill = "blue", outline = "black")
-    			arena.update()
-    			time.sleep(0.1)
-    	else:
-    			generate_big_obstacle(arena_list, arena)
-    	return arena_list
+		x_list = random.randint(0,24)
+		y_list = random.randint(0,24)
+		if(x_list < 23 and y_list < 23 and arena_list[x_list][y_list] == 0 and arena_list[x_list + 1][y_list] == 0 and arena_list[x_list][y_list + 1] == 0 and arena_list[x_list + 1][y_list + 1] == 0):
+				arena_list[x_list][y_list] = 2
+				arena_list[x_list + 1][y_list] = 2
+				arena_list[x_list][y_list + 1] = 2
+				arena_list[x_list + 1][y_list + 1] = 2
+				x_coord0 = x_list * 20
+				y_coord0 = y_list * 20
+				x_coord1 = x_coord0 + 40
+				y_coord1 = y_coord0 + 40
+				global big_obstacle
+				big_obstacle = PhotoImage(file = 'obstacle.gif')
+				obstacle = arena.create_image(x_coord0, y_coord0, image = big_obstacle, anchor = NW)
+				arena.update()
+				time.sleep(0.1)
+		else:
+				generate_big_obstacle(arena_list, arena)
+		return arena_list
 def generate_traffic_light_object(arena_list,arena,traffic_list):
-    print "traffics light"
-    x_list = random.randint(0,24)
-    y_list = random.randint(0,24)
-    traffic_list.append(x_list)
-    traffic_list.append(y_list)
-    if (x_list< 22 and arena_list[x_list][y_list] == 0):
-    	arena_list[x_list][y_list] = 3
-    	arena_list[x_list + 1][y_list] = 3
-    	arena_list[x_list + 2][y_list] = 3
-    	x_coord0 = x_list * 20
-    	y_coord0 = y_list * 20
-    	x_coord1 = x_coord0 + 20
-    	y_coord1 = y_coord0 + 20
-    	obstacle = arena.create_rectangle(x_coord0, y_coord0, x_coord1, y_coord1, fill = "green")
-    	arena.update()
-    else:
-    	generate_traffic_light_object(arena_list,arena,traffic_list)
-    
+	print "traffics light"
+	x_list = random.randint(0,24)
+	y_list = random.randint(0,24)
+	traffic_list.append(x_list)
+	traffic_list.append(y_list)
+	if (x_list< 22 and arena_list[x_list][y_list] == 0):
+		arena_list[x_list][y_list] = 3
+		arena_list[x_list + 1][y_list] = 3
+		arena_list[x_list + 2][y_list] = 3
+		x_coord0 = x_list * 20
+		y_coord0 = y_list * 20
+		x_coord1 = x_coord0 + 60
+		y_coord1 = y_coord0 + 20
+		global traffic_light_green_obstacle
+		traffic_light_green_obstacle = PhotoImage(file = 'traffic_light_green.gif')
+		obstacle = arena.create_image(x_coord0, y_coord0, image = traffic_light_green_obstacle, anchor = NW)
+		arena.update()
+	else:
+		generate_traffic_light_object(arena_list,arena,traffic_list)
+	
 def generate_mud_object(arena_list,arena):
-    print "mud"
-    x_list = random.randint(0,24)
-    y_list = random.randint(0,24)
-    if(x_list < 23 and y_list < 23 and arena_list[x_list][y_list] == 0 and arena_list[x_list + 1][y_list] == 0 and arena_list[x_list][y_list + 1] == 0 and arena_list[x_list + 1][y_list + 1] == 0):
-    		arena_list[x_list][y_list] = 4
-    		arena_list[x_list + 1][y_list] = 4
-    		arena_list[x_list][y_list + 1] = 4
-    		arena_list[x_list + 1][y_list + 1] = 4
-    		x_coord0 = x_list * 20
-    		y_coord0 = y_list * 20
-    		x_coord1 = x_coord0 + 40
-    		y_coord1 = y_coord0 + 40
-    		obstacle = arena.create_rectangle(x_coord0, y_coord0, x_coord1, y_coord1, fill = "brown", outline = "brown")
-    		arena.update()
-    		time.sleep(0.1)
-    else:
-    	generate_mud_object(arena_list,arena)
+	print "mud"
+	x_list = random.randint(0,24)
+	y_list = random.randint(0,24)
+	if(x_list < 23 and y_list < 23 and arena_list[x_list][y_list] == 0 and arena_list[x_list + 1][y_list] == 0 and arena_list[x_list][y_list + 1] == 0 and arena_list[x_list + 1][y_list + 1] == 0):
+			arena_list[x_list][y_list] = 4
+			arena_list[x_list + 1][y_list] = 4
+			arena_list[x_list][y_list + 1] = 4
+			arena_list[x_list + 1][y_list + 1] = 4
+			x_coord0 = x_list * 20
+			y_coord0 = y_list * 20
+			x_coord1 = x_coord0 + 40
+			y_coord1 = y_coord0 + 40
+			global mud_obstacle
+			mud_obstacle = PhotoImage(file = 'mud.gif')
+			obsactle = arena.create_image(x_coord0, y_coord0, image = mud_obstacle, anchor = NW)
+			arena.update()
+			time.sleep(0.1)
+	else:
+		generate_mud_object(arena_list,arena)
 def generate_speed_boost_object(arena_list,arena):
-    print "speed boost"
-    x_list = random.randint(0,24)
-    y_list = random.randint(0,24)
-    if (arena_list[x_list][y_list] == 0):
-    	arena_list[x_list][y_list] = 5
-    	x_coord0 = x_list * 20
-    	y_coord0 = y_list * 20
-    	x_coord1 = x_coord0 + 20
-    	y_coord1 = y_coord0 + 20
-    	obstacle = arena.create_rectangle(x_coord0, y_coord0, x_coord1, y_coord1, fill = "orange", outline = "green")
+	print "speed boost"
+	x_list = random.randint(0,24)
+	y_list = random.randint(0,24)
+	if (arena_list[x_list][y_list] == 0):
+		arena_list[x_list][y_list] = 5
+		x_coord0 = x_list * 20
+		y_coord0 = y_list * 20
+		x_coord1 = x_coord0 + 20
+		y_coord1 = y_coord0 + 20
+		global speed_boost_obstacle
+		speed_boost_obstacle = PhotoImage(file = 'speed_boost.gif')
+		obstacle = arena.create_image(x_coord0, y_coord0, image = speed_boost_obstacle, anchor = NW)
  
-    	arena.update()
-    else:
-    	generate_speed_boost_object(arena_list,arena)
+		arena.update()
+	else:
+		generate_speed_boost_object(arena_list,arena)
 def genertate_red_traffic_light(arena_list,arena): # this function gives the traffic light locations and it turns the traffic light red
-    print "red"
-    if call_num == 1:
-    	traffic_x_coord0 = traffic_list[1]
-    	traffic_y_coord0 = traffic_list[2]
+	print "red"
+	if call_num == 1:
+		traffic_x_coord0 = traffic_list[1]
+		traffic_y_coord0 = traffic_list[2]
 
-    	traffic_light_change(traffic_x_coord0,traffic_y_coord0)
-    elif call_num == 2:
-    	traffic_x_coord0 = traffic_list[3]
-    	traffic_y_coord0 = traffic_list[4]
-    elif call_num == 3:
-    	traffic_x_coord0 = traffic_list[5]
-    	traffic_y_coord0 = traffic_list[6]
-    else:
-    	print "no more in list"
+		traffic_light_change(traffic_x_coord0,traffic_y_coord0)
+	elif call_num == 2:
+		traffic_x_coord0 = traffic_list[3]
+		traffic_y_coord0 = traffic_list[4]
+	elif call_num == 3:
+		traffic_x_coord0 = traffic_list[5]
+		traffic_y_coord0 = traffic_list[6]
+	else:
+		print "no more in list"
 
-    call_num = call_num + 1
+	call_num = call_num + 1
 def traffic_light_change(traffic_x_coord0,traffic_y_coord0):
-    traffic_x_coord1 = traffic_x_coord0 + 1
-    traffic_y_coord1 = traffic_y_coord0 + 1
+	traffic_x_coord1 = traffic_x_coord0 + 1
+	traffic_y_coord1 = traffic_y_coord0 + 1
 
-    obstacle_red_traffic = arena.create_rectangle(traffic_x_coord0, traffic_y_coord0, traffic_x_coord1, traffic_y_coord1, fill = "red", outline = "black")
-    # we could either do a pause or change robot speed to 0 here 
-    arena.delete(obstacle_red_traffic)
-    arena.update()
+	global traffic_light_red
+	traffic_light_red = PhotoImage(file = 'traddif_light_red.gif')
+	obstacle = arena.create_image(traffic_x_coord0, traffic_y_coord0, image = traffic_light_red, anchor = NW)
+	# we could either do a pause or change robot speed to 0 here 
+	arena.delete(obstacle_red_traffic)
+	arena.update()
+def generate_finish_area():
+	x_list = random.randint(0,24)
+	y_list = random.randint(0,24)
+	if (arena_list[x_list][y_list] == 0):
+		arena_list[x_list][y_list] = 7
+		x_coord0 = x_list * 20
+		y_coord0 = y_list * 20
+		x_coord1 = x_coord0 + 20
+		y_coord1 = y_coord0 + 20
+
 def advanced_init():
-    	window2 = Tk()
-    	arena = Canvas(window2, width = 500, height = 500, bg = 'white') # generates a canvas of 500px x 500px for the arena
-    	for i in range(0,5):
-    		generate_big_obstacle(arena_list, arena)
-    		arena.update()
-    	for i in range(0,3):
-    		generate_mud_object(arena_list,arena)
-    		arena.update()
-    	for i in range(0,3):
-    		generate_traffic_light_object(arena_list,arena,traffic_list)
-    		arena.update()
-    	for i in range(0,3):
-    		generate_speed_boost_object(arena_list,arena)
-    		arena.update()
-    	for i in range(0,30):
-    		generate_obstacle(arena_list, arena)
-    		arena.update()
-    	print arena_list
+		window2 = Toplevel()
+		arena = Canvas(window2, width = 500, height = 500, bg = 'white') # generates a canvas of 500px x 500px for the arena
+		for i in range(0,5):
+			generate_big_obstacle(arena_list, arena)
+			arena.update()
+		for i in range(0,3):
+			generate_mud_object(arena_list,arena)
+			arena.update()
+		for i in range(0,3):
+			generate_traffic_light_object(arena_list,arena,traffic_list)
+			arena.update()
+		for i in range(0,3):
+			generate_speed_boost_object(arena_list,arena)
+			arena.update()
+		for i in range(0,30):
+			generate_obstacle(arena_list, arena)
+			arena.update()
+		print arena_list
 
-        import robot
+		import robot
 
-        robot1 = robot.Robot()
-        robot1.speed = speed2
-        robot1.spawn_robot(arena)
-        # robot1.spawn_robot(robot1,arena)
-        print arena_list
-        
-        direction = 0
+		robot1 = robot.Robot()
+		robot1.speed = speed2
+		robot1.spawn_robot(arena)
+		# robot1.spawn_robot(robot1,arena)
+		print arena_list
+		
+		direction = 0
 
-        arena.pack()
+		arena.pack()
+		arena.update()
+		arena.update_idletasks()
+		go = True
 
-        arena.update_idletasks()
-        go = True
+		while go == True:
+			print go
+			print  arena.coords(robot1)
+			if robot1.coords[2] >= 480:
+				print "hit a wall first"
+				robot1.random_direction(robot1.direction)
+				robot1.move(arena)
+			elif robot1.coords[0] < 20:
+				print "hit a wall second"
+				robot1.random_direction(robot1.direction)
+				robot1.move(arena)
+			elif robot1.coords[1] >= 480:
+				print "hit a wall third"
+				robot1.random_direction(robot1.direction)
+				robot1.move(arena)
+			elif robot1.coords[1] <= 20:
+				print "hit a wall fourth"
+				robot1.random_direction(robot1.direction)
+				robot1.move(arena)
+			robot1.check()
+			robot1.move(arena)
+			arena.update()
+			time.sleep(0.1)
+		print  arena.coords(robot1)
+		print go
+		global speed_boost_obstacle
+		"""
+		speed_boost_obstacle = PhotoImage(file = 'speed_boost.gif')
+		arena.create_image(10, 10, image = speed_boost_obstacle, anchor = NW)
+		arena.pack()
+		arena.update()
+		print " this is a test"
+		"""
+		robot.direction = direction
+		window2.mainloop() # runs everything
 
-        while go == True:
-            print go
-            print  arena.coords(robot1)
-            if robot1.coords[2] >= 480:
-                print "hit a wall first"
-                robot1.random_direction(robot1.direction)
-                robot1.move(arena)
-            elif robot1.coords[0] < 20:
-                print "hit a wall second"
-                robot1.random_direction(robot1.direction)
-                robot1.move(arena)
-            elif robot1.coords[1] >= 480:
-                print "hit a wall third"
-                robot1.random_direction(robot1.direction)
-                robot1.move(arena)
-            elif robot1.coords[1] <= 20:
-                print "hit a wall fourth"
-                robot1.random_direction(robot1.direction)
-                robot1.move(arena)
-            robot1.check()
-            robot1.move(arena)
-            arena.update()
-            time.sleep(0.1)
-        print  arena.coords(robot1)
-        print go
 
-        window2.mainloop() # runs everything
-
-
-
-
-        robot.direction = direction
